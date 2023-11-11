8dfc46b4a254ed5557aa8556d6b59e70e271c26e
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2023-11-11 20:04:58 +0300

    aiogram echo 1

--- main.py
+++ main.py
@@ -2,33 +2,34 @@ import json
 import requests
 import time
 
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     model_config = SettingsConfigDict(env_file=".env")
-    
-API_URL = "https://api.telegram.org/bot"
-TEXT = "Update received"
-MAX_COUNTER = 100
 
-offset = -2
-counter = 0
-chat_id: int
+
 settings = Settings()
 
-while counter < MAX_COUNTER:
-    print("attempt=", counter)
-    updates = requests.get(f"{API_URL}{settings.BOT_TOKEN}/getUpdates?offset={offset+1}").json()
+bot = Bot(token=settings.BOT_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer("Hi!\nI'm echoBot")
+
+@dp.message(Command(commands=["help"]))
+async def process_help_command(message: Message):
+    await message.answer("Type anything and I'll send it back")
     
-    print(json.dumps(updates, separators=(",", "-"), indent="  "))
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
     
-    if updates['result']:
-        for result in updates['result']:
-            offset = result["update_id"]
-            chat_id = result["message"]["from"]["id"]
-            requests.get(f"{API_URL}{settings.BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={result['message']['from']['first_name']} says {result['message']['text']}")
-            
-    time.sleep(5)
-    counter += 1
+if __name__ == "__main__":
+    dp.run_polling(bot)
