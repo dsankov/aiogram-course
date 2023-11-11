fd6f993b03b9aae7de1d7dfc7db5298c7910f1c6
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2023-11-11 15:08:04 +0300

    .env settings used

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello")
+import requests
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    
+    model_config = SettingsConfigDict(env_file=".env")
+    
+
+settings = Settings()
+print(settings.BOT_TOKEN)
+
