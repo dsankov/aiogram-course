bf94e0017620e81e236652f171a1ffc90114bbbe
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2023-11-11 19:48:04 +0300

    telegramAPI usage example

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import json
 import requests
+import time
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
-    
     model_config = SettingsConfigDict(env_file=".env")
     
+API_URL = "https://api.telegram.org/bot"
+TEXT = "Update received"
+MAX_COUNTER = 100
 
+offset = -2
+counter = 0
+chat_id: int
 settings = Settings()
-print(settings.BOT_TOKEN)
 
+while counter < MAX_COUNTER:
+    print("attempt=", counter)
+    updates = requests.get(f"{API_URL}{settings.BOT_TOKEN}/getUpdates?offset={offset+1}").json()
+    
+    print(json.dumps(updates, separators=(",", "-"), indent="  "))
+    
+    if updates['result']:
+        for result in updates['result']:
+            offset = result["update_id"]
+            chat_id = result["message"]["from"]["id"]
+            requests.get(f"{API_URL}{settings.BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={result['message']['from']['first_name']} says {result['message']['text']}")
+            
+    time.sleep(5)
+    counter += 1
