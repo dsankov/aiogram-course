7786f13bb78429fc4d7cf5e8f7724a0e004bf80a
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2023-11-17 18:29:27 +0300

    main function and no decorators

---
+++ config.py
@@ -0,0 +1,8 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    API_URL: str
+
+    model_config = SettingsConfigDict(env_file=".env")
--- main.py
+++ main.py
@@ -7,18 +7,20 @@ from aiogram.filters import Command
 from aiogram.types import ContentType, Message
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+from config import Settings
 
-class Settings(BaseSettings):
-    BOT_TOKEN: str
-    API_URL: str
+def main()  -> None:
+    settings = Settings()
+    bot = Bot(token=settings.BOT_TOKEN)
+    dp = Dispatcher()
+    dp.message.register(send_photo_echo, F.photo)
+    dp.message.register(process_sent_voice, F.voice)
+    dp.message.register(process_sent_sticker, F.sticker)
+    dp.message.register(send_echo_for_default)
+    dp.message.register(process_start_command,F.command=="/start")
+    dp.run_polling(bot)
 
-    model_config = SettingsConfigDict(env_file=".env")
 
-
-settings = Settings()
-
-bot = Bot(token=settings.BOT_TOKEN)
-dp = Dispatcher()
 
 
 def execute_telegram_api_request(settings: Settings, command: str, **params):
@@ -33,13 +35,13 @@ def print_aiogram_Message(message: Message) -> None:
     print(message.model_dump_json(indent=4, exclude_none=True))
 
 
-@dp.message(Command(commands=["start"]))
+# @dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     # print_aiogram_Message(message)
     await message.answer("Hi!\nI'm echoBot")
 
 
-@dp.message(Command(commands=["help"]))
+# @dp.message(Command(commands=["help"]))
 async def process_help_command(message: Message):
     await message.answer("Type anything and I'll send it back")
 
@@ -59,17 +61,10 @@ async def process_sent_sticker(message: Message):
     await message.answer(text="sticker received")
 
 
-dp.message.register(send_photo_echo, F.photo)
-dp.message.register(process_sent_voice, F.voice)
-dp.message.register(process_sent_sticker, F.sticker)
-
-
-@dp.message()
-async def send_echo(message: Message):
+async def send_echo_for_default(message: Message):
     print_aiogram_Message(message)
     await message.reply(text="received")
 
 
 if __name__ == "__main__":
-    dp.run_polling(bot)
-
+    main()
